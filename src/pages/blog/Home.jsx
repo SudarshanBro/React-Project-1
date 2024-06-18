bc3618f0cc580f65c57edf12a574105884d54a2b
Sudarshan_0126 <128712927+SudarshanBro@users.noreply.github.com>
2024-06-18 21:44:06 +0545

    Add files via upload

---
+++ src/pages/blog/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Layout from '../../components/layout/Layout'
+import Card from './components/card/Card'
+import axios from 'axios'
+import { baseUrl } from '../../config'
+
+const Home = () => {
+  const [blogs,setBlogs] = useState([])
+  const fetchBlogs = async ()=>{
+    const response = await axios.get(`${baseUrl}/blog`)
+    if(response.status === 200){
+      setBlogs(response.data.data)
+    }
+  }
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
+  return (
+   <Layout>
+  <div className='flex flex-wrap justify-center space-x-5 mt-6'>
+    {
+      blogs.length > 0 && blogs.map((blog)=>{
+        console.log(blog)
+        return (
+          <Card blog={blog} />
+        )
+      })
+    }
+  
+
+  </div>
+   </Layout>
+  )
+}
+
+export default Home
